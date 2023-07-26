1c733aa04b11058d7c986da2aa2b157cdf227d41
Twinkal Narnaware <94154988+twinkleUniverse@users.noreply.github.com>
2023-07-26 17:01:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 //your code here
+function removeArticles(text) {
+  const articles = ["a", "an", "the"];
+  const words = text.split(" ");
+  return words.filter(word => !articles.includes(word.toLowerCase())).join(" ");
+}
 
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+let modifiedArray = touristSpots.map(removeArticles);
+let ans=[];
+
+let sorted=modifiedArray.sort();
+for(let i=0;i<touristSpots.length;i++){
+	let words=removeArticles(touristSpots[i])
+	let indexSort=sorted.indexOf(words);
+    ans[indexSort]=touristSpots[i];
+}
+console.log(ans);
