16d7d75203ff4c02802a4c661bcec2e3c3bfd658
Twinkal Narnaware <94154988+twinkleUniverse@users.noreply.github.com>
2023-07-27 14:26:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,19 +2,18 @@
 let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 const compar=(nameA,nameB)=>{
 	const articles=['a','an','the'];
-	const removeArticles=(name)=>{
-		const words=name.split(' ');
-		return words.filter(word=>!articles.includes(word.toLowerCase())).join(' ');
-	   const bandNameA=removeArticles(nameA)
+	const removeArticles=(name)=> {
+	const words=name.split(' ');
+	return words.filter(word=>!articles.includes(word.toLowerCase())).join(' ');
+	} 
+		const bandNameA=removeArticles(nameA)
 		const bandNameB=removeArticles(nameB);
 		return bandNameA.localeCompare(bandNameB);
-	}
-
-	const sorted=touristSpots.sort(compar);
-	const list=document.getElementById("band");
-	for(let i=0;i<sorted.length;i++){
-		const listTag=document.createElement("li");
-		listTag.innerContent=sorted[i];
-		list.appendChild(listTag);
-	}
+}
+const sorted=touristSpots.sort(compar);
+const list=document.getElementById("band");
+for(let i=0;i<sorted.length;i++){
+  const listTag=document.createElement("li");
+  listTag.innerContent=sorted[i];
+  list.appendChild(listTag);
 }
