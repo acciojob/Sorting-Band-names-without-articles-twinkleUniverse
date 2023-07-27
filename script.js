ca90a8081544dfc67b2a51c4df24a5248bbaa119
Twinkal Narnaware <94154988+twinkleUniverse@users.noreply.github.com>
2023-07-27 14:08:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,1 @@
 //your code here/
-function removeArticles(text) {
-  const articles = ["a", "an", "the"];
-  const words = text.split(" ");
-  return words.filter(word => !articles.includes(word.toLowerCase())).join(" ");
-}
-
-// Example array of strings
- originalArray = ['Tajmahal', 'Tajmahal', 'Tajmahal'];
-
-const modifiedArray = originalArray.map(removeArticles);
-const sortedArray = modifiedArray.slice().sort((a, b) => a.localeCompare(b, undefined, { sensitivity: 'base' }));
-
-const outputArray = sortedArray.map((modifiedStr) => {
-  const index = modifiedArray.indexOf(modifiedStr);
-	console.log(index)
-  return originalArray[index];
-});
-console.log(outputArray);
